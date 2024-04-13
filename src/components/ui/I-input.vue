ceda3833f8da5d8d69611c9af484321ebced88f7
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 12:17:00 +0300

    feat: create input component and style

---
+++ src/components/ui/I-input.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+export interface IFormField {
+  id?: string;
+  label?: string;
+  type?: string;
+  name: string;
+  placeholder?: string;
+}
+
+const model = defineModel<string>();
+
+const { id, name } = withDefaults(defineProps<IFormField>(), {
+  id: 'form-field',
+  type: 'text',
+  placeholder: 'Type text',
+  label: ''
+});
+
+const fieldId = `${id}-${name}`;
+</script>
+
+<template>
+  <div class="form-field">
+    <label v-if="label?.trim()" :for="fieldId" class="form-label">{{ label }}</label>
+    <input
+      :id="fieldId"
+      v-model.trim="model"
+      :name="name"
+      class="form-input"
+      :placeholder="placeholder"
+      :type="type"
+    />
+  </div>
+</template>
