359cdbf2f15d38216a37f6b6b11cf55e7d938076
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 17:53:10 +0300

    feat: create constants

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const API_URL = 'https://jsonplaceholder.typicode.com';
+
+export const sortOptions = [
+  {
+    value: 'first_name',
+    name: 'By Name'
+  },
+  {
+    value: 'id',
+    name: 'By Id'
+  }
+];
