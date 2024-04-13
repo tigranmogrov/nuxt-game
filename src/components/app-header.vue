d4af1a8b39a3ccef5e69c714330a72a107e5203c
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 17:57:48 +0300

    feat: create header component and style

---
+++ src/components/app-header.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import IIconExit from '@/components/icons/i-icon-exit.vue';
+import IButton from '@/components/ui/i-button.vue';
+import { useUserStore } from '@/stores/user.store';
+import { useRouter } from 'vue-router';
+
+const { logoutUser } = useUserStore();
+const router = useRouter();
+const logout = () => {
+  logoutUser();
+  router.push({ name: 'login' });
+};
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__inner">
+      <div class="header__info"></div>
+      <i-button @click="logout">
+        Exit
+        <i-icon-exit />
+      </i-button>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
