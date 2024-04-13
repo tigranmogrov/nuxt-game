617353c22529035d85ca4baef03eb6935dc3c896
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 19:15:06 +0300

    feat(homeView): add user info components

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IUserInfo from '@/components/i-user-info.vue';
+</script>
 <template>
   <div class="container">
+    <i-user-info />
     <div class="home__content"></div>
   </div>
 </template>
