e3459ca4f7a786f5dbe1ccbcc2e5a624de4eb509
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 15:38:11 +0300

    feat: create layouts empty and main

---
+++ src/layouts/empty-layout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main login">
+    <slot />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/layouts/main-layout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main">
+    <slot />
+  </main>
+</template>
+
+<style scoped></style>
