62fadbb823a46fad1033491a170899221abaedaa
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 16:20:36 +0300

    refactor:  lint fixed

--- src/layouts/main-layout.vue
+++ src/layouts/main-layout.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from '@/components/app-header.vue';
+</script>
 
 <template>
+  <app-header />
   <main class="main">
     <slot />
   </main>
