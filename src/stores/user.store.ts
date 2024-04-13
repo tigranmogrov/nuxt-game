d9cb483af9b75c15547963fbc7e83c5cd7413b15
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 17:38:27 +0300

    fear(store): create user store add login user action

---
+++ src/stores/user.store.ts
@@ -0,0 +1,52 @@
+import { API } from '@/api';
+import { useAuthStore } from '@/stores/auth.store';
+import type { IUser } from '@/types';
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+
+export const useUserStore = defineStore('user', () => {
+  const authStore = useAuthStore();
+  const user = ref<IUser | null>(null);
+  const error = ref<string | null>(null);
+
+  const getError = computed(() => error.value);
+  const getUser = computed(() => user.value);
+
+  const loginUser = async (userName: string, phone: string): Promise<void> => {
+    try {
+      const { data } = await API.get<IUser[]>('/users');
+      const userData = data.find((userData) => {
+        return (
+          userData.username.toLowerCase() === userName.toLowerCase() && userData.phone === phone
+        );
+      });
+      if (userData) {
+        setUser(userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+      } else {
+        setError();
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+
+  const setUser = (payload: IUser): void => {
+    user.value = payload;
+    authStore.setAuth(true);
+  };
+
+  const setError = (): void => {
+    error.value = 'Login error';
+    throw new Error('User not exist');
+  };
+
+  return {
+    user,
+    error,
+    getError,
+    getUser,
+    loginUser,
+    setUser
+  };
+});
