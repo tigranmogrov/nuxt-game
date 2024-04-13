36ebfbbf62b82238e6642260f8623677de7cb634
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 17:56:56 +0300

    feat: add action getCurrentUser

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import EmptyLayout from '@/layouts/empty-layout.vue';
 import MainLayout from '@/layouts/main-layout.vue';
+import { useUserStore } from '@/stores/user.store';
 import { computed, reactive, shallowRef } from 'vue';
 import { RouterView, useRoute } from 'vue-router';
 
 const route = useRoute();
-
+const userStore = useUserStore();
 const layouts = reactive<{ [key: string]: any }>({
   main: shallowRef(MainLayout),
   empty: shallowRef(EmptyLayout)
 });
 
 const getLayout = computed(() => layouts[String(route.meta.layout)]);
+
+userStore.getCurrentUser();
 </script>
 
 <template>
