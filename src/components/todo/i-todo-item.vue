5a5c76195e7ca28f6f449f792b326a30e3020b60
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 18:09:27 +0300

    feat: create todo component and style

---
+++ src/components/todo/i-todo-item.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import IButton from '@/components/ui/i-button.vue';
+import { useTodosStore } from '@/stores/todos.store';
+import { ActionEnum, type ITodo } from '@/types';
+import { computed, defineEmits } from 'vue';
+
+const todoStore = useTodosStore();
+
+const { post } = defineProps<{ post: ITodo }>();
+
+const emits = defineEmits(['updateFavorites']);
+
+const isFavorite = computed(() => todoStore.getFavorites.includes(post.id));
+
+const updateFavorites = () => {
+  const action = isFavorite.value ? ActionEnum.REMOVE_FAVORITE : ActionEnum.ADD_FAVORITE;
+  emits('updateFavorites', { action, id: post.id });
+};
+</script>
+
+<template>
+  <li class="todo__item">
+    <h3 class="todo__title">
+      {{ post.title }}
+    </h3>
+    <i-button @click="updateFavorites">
+      {{ isFavorite ? 'Remove from Favorites' : 'Add to Favorites' }}
+    </i-button>
+  </li>
+</template>
---
+++ src/components/todo/i-todo-list.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import ITodoItem from '@/components/todo/i-todo-item.vue';
+import { filterByStatus, searchByText, filterByUserId } from '@/composable/filters';
+import { useTodosStore } from '@/stores/todos.store';
+import { ActionEnum, type IUpdateFavoriteAction } from '@/types';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+
+const { filtersOption } = defineProps<{
+  filtersOption: { [key: string]: string };
+}>();
+
+const todoStore = useTodosStore();
+
+const { getTodos: todos } = storeToRefs(todoStore);
+
+const filteredTodos = computed(() => {
+  if (!todos.value) return [];
+
+  const searchByTitle = searchByText(todos.value, filtersOption.text);
+  const filteredByUserId = filterByUserId(searchByTitle, filtersOption.userId);
+  return filterByStatus(filteredByUserId, filtersOption.status);
+});
+
+const addToFavorite = (id: number) => {
+  const favorites = todoStore.getFavorites ?? [];
+  localStorage.setItem('favorites', JSON.stringify([...favorites, id]));
+  todoStore.setTodoFavorites();
+};
+
+const removeFavorite = (id: number) => {
+  const newFavorites = todoStore.getFavorites.filter((favorite) => favorite !== id);
+  localStorage.setItem('favorites', JSON.stringify([...newFavorites]));
+  todoStore.setTodoFavorites();
+};
+
+const updateFavorites = ({ action, id }: IUpdateFavoriteAction) => {
+  switch (action) {
+    case ActionEnum.ADD_FAVORITE:
+      addToFavorite(id);
+      break;
+    case ActionEnum.REMOVE_FAVORITE:
+      removeFavorite(id);
+      break;
+  }
+};
+</script>
+
+<template>
+  <div class="todo">
+    <ul v-if="filteredTodos.length" class="todo__list">
+      <i-todo-item
+        v-for="todo in filteredTodos"
+        :key="todo.id"
+        :post="todo"
+        @update-favorites="updateFavorites"
+      />
+    </ul>
+    <h3 v-else class="todo__title">Ooops... Todos is empty</h3>
+  </div>
+</template>
