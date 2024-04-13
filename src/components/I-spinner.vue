12c8a4703fa8c858ade95ac259e850bd63172b9d
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 12:10:47 +0300

    feat: create a spinner component and style

---
+++ src/components/I-spinner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="spinner">
+    <p>Your data is loading...</p>
+    <div class="spinner__inner">
+      <span class="spinner__line"></span>
+      <span class="spinner__line"></span>
+      <span class="spinner__line"></span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
