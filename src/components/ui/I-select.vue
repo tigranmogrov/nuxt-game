18d6c1b1cdb491bd8de2ab97e457c5a6015a01d8
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 12:16:52 +0300

    feat: create a select component and style

---
+++ src/components/ui/I-select.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+export interface ISelectOptions {
+  value: string;
+  name: string;
+}
+export interface IFormSelect {
+  id?: string;
+  label?: string;
+  name: string;
+  options: ISelectOptions[];
+}
+
+const model = defineModel<string>();
+
+const { name, id } = withDefaults(defineProps<IFormSelect>(), {
+  id: 'form-field',
+  label: ''
+});
+
+const fieldId = `${id}-${name}`;
+</script>
+
+<template>
+  <div class="form-field">
+    <label v-if="label?.trim()" :for="fieldId" class="form-label">{{ label }}</label>
+    <select :id="fieldId" v-model="model" :name="name" class="form-select">
+      <option v-for="option in options" :key="option.value" :value="option.value">
+        {{ option.name }}
+      </option>
+    </select>
+  </div>
+</template>
