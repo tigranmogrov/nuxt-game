03876d43d01f0c08f384f1f442612b9e03a6d53f
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-13 15:51:30 +0300

    feat: create App and add dynamic layouts

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import EmptyLayout from '@/layouts/empty-layout.vue';
+import MainLayout from '@/layouts/main-layout.vue';
+import { computed, reactive, shallowRef } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const layouts = reactive<{ [key: string]: any }>({
+  main: shallowRef(MainLayout),
+  empty: shallowRef(EmptyLayout)
+});
+
+const getLayout = computed(() => layouts[String(route.meta.layout)]);
+</script>
+
+<template>
+  <component :is="getLayout">
+    <router-view />
+  </component>
+</template>
