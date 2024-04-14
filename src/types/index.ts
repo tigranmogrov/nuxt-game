e0d6a7c463b6030bc757b3b1afec292faf509e71
Tigran Mogrov <tigranmogrov@gmail.com>
2024-04-14 13:19:33 +0300

    refactor(types):   todo export path and add filters.type export

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 export * from './auth/auth.types';
 export * from './user/user.types';
-export * from '@/types/todo/todo.types';
+export * from './todo/todo.types';
 export * from './form/validation.types';
+export * from './form/filters.type';
